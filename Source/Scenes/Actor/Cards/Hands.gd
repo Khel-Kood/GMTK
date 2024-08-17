5b73eadb224d293907a17dcfb81a79dc8d756c60
Wadedacoder <devm4966@gmail.com>
2024-08-17 23:38:20 +0530

    Adding entities management

--- Source/Scenes/Actor/Cards/Hands.gd
+++ Source/Scenes/Actor/Cards/Hands.gd
@@ -5,7 +5,9 @@ extends Node
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-    pass # Replace with function body.
+  #Init array to make all entities
+    pass
+    #pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
---
+++ Source/Scenes/Main.gd
@@ -0,0 +1,22 @@
+extends Node2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+  var actorScene = preload("res://Source/Scenes/Actor/Actor.tscn")
+  var actorInstance = actorScene.instantiate()
+  actorInstance.allignment = actorInstance.Allignment.Player
+  actorInstance.totalHealth = 60
+  actorInstance.position = (Vector2(180, 220))
+  add_child(actorInstance)
+  var enemy = actorScene.instantiate()
+  enemy.allignment = actorInstance.Allignment.Enemy
+  enemy.totalHealth = 60
+  enemy.position = (Vector2(1000, 220))
+  add_child(enemy)
+  #pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+  pass
