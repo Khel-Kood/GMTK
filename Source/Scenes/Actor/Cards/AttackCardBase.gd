7bd8d3b57f41028b71d88a8fac48d8c0bd8dca6f
RaghavSakhuja <raghav21274@iiitd.ac.in>
2024-08-17 23:40:47 +0530

    base

--- Source/Scenes/Actor/Cards/AttackCardBase.gd
+++ Source/Scenes/Actor/Cards/AttackCardBase.gd
@@ -1,12 +1,12 @@
 extends "res://Source/Scenes/Actor/Cards/CardBase.gd"
 
 func _ready() -> void:
-	# Set the label nodes here
-	cardNameLabel = $TextureButton/CardNameLabel
-	cardNumberLabel = $TextureButton/CardNumberLabel
+    # Set the label nodes here
+    cardNameLabel = $TextureButton/CardNameLabel
+    cardNumberLabel = $TextureButton/CardNumberLabel
 
-	# Set the card's name and number
-	setCardName("Fireball")
-	setCardNumber(5)
+    # Set the card's name and number
+    setCardName("Fireball")
+    setCardNumber(5)
 
-	# If you have additional setup, handle it here
+    # If you have additional setup, handle it here
