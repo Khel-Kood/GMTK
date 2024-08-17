f8f883884ceaf3f8df1534cb1432851f0ceed130
Wadedacoder <devm4966@gmail.com>
2024-08-17 20:43:47 +0530

    Chal gay benchod

--- Source/Scenes/Actor/Actor.gd
+++ Source/Scenes/Actor/Actor.gd
@@ -1,5 +1,5 @@
 extends Node2D
-
+class_name Actor
 
 enum Allignment{
   Player, Enemy
@@ -10,17 +10,42 @@ enum Allignment{
 @export var totalHealth : int = 50
 @export var allignment : Allignment = Allignment.Player;
 
+signal deadEnemy
+signal deadPlayer
+
 var health := totalHealth
 
+func enemyDead():
+  print("Enemy Died")
+  #Delete the entity
+  self.queue_free()
+  
 
+func onHurt(damage: int):
+  health = clampi(health - damage, 0, totalHealth)
+  if(health <= 0):
+    #implement you ded
+    if (allignment == Allignment.Enemy):
+      print("Enemy maar bc")
+      deadEnemy.emit()
+      enemyDead()
+    else:
+      deadPlayer.emit()
+    
 
 func _ready():
+  #Taking initial Health; can be changed via reference
   health = totalHealth
   #print(health)
   
+  #connect with dead Enemy Signal
+  #deadEnemy.connect(enemyDead())
+  
 func _input(event):
+  #Todo change with signals
   if(event.is_action_pressed("down")):
-    health -= 1
+     #print("down")
+     onHurt(5)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta): 
