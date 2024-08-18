6143fd26113cde5653aab03382d27aca385b6706
RaghavSakhuja <raghav21274@iiitd.ac.in>
2024-08-18 15:21:38 +0530

    getting cards from deck and rng

--- Source/Scenes/Actor/Cards/Hands.gd
+++ Source/Scenes/Actor/Cards/Hands.gd
@@ -3,6 +3,7 @@ extends Node
 #var InHand: array ( the cards in hand ) 
 #var dekh: DECK
 var InHand: Array[Card] = []
+@onready var deck = $Deck
 
 func _ready() -> void:
     print("Hand Ready")
@@ -13,21 +14,24 @@ func _ready() -> void:
         card.connect("cardSelect", Callable(self,"deSelectAll"))
         
 func createHand():
-    var tempCard = preload("res://Source/Scenes/Actor/Cards/ArrowAttackCard.tscn")
-    InHand.append(tempCard.instantiate())
-
-    tempCard = preload("res://Source/Scenes/Actor/Cards/AttackCard.tscn")
-    InHand.append(tempCard.instantiate())
-
-    tempCard = preload("res://Source/Scenes/Actor/Cards/SwordAttackCard.tscn")
-    InHand.append(tempCard.instantiate())
+    print(deck)
+    deck.addCard(1)
+    deck.addCard(3)
+    deck.addCard(2)
+    
+    var cardScenes = deck.drawCards()
+    for i in range(cardScenes.size()):
+        var cardInstance = cardScenes[i].instantiate()
+        InHand.append(cardInstance)
+    
+    
     
 func showCards():
     print(InHand.size())
     for i in range(InHand.size()):
         print(i)
         var cardInstance = InHand[i]
-        cardInstance.position = Vector2(50 + i * 150, 50)
+        cardInstance.position = Vector2(i * 150, 50)
         add_child(cardInstance)
     
 func _process(delta):
@@ -39,3 +43,10 @@ func deSelectAll():
         var card = InHand[i]
         card.deSelect()
 
+
+func getSelectedCard() -> Card:
+    for i in range(InHand.size()):
+        var card = InHand[i]
+        if card.isSelected:
+            return card
+    return null
