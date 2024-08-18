7798e87930c49d6a0d42b5c7bbf4e0ef6a046801
RaghavSakhuja <raghav21274@iiitd.ac.in>
2024-08-18 14:43:49 +0530

    card selection and deselection for every card

--- Source/Scenes/Actor/Cards/ArrowAttack.gd
+++ Source/Scenes/Actor/Cards/ArrowAttack.gd
@@ -1,17 +1,18 @@
 extends "res://Source/Scenes/Actor/Cards/CardBase.gd"
 
 func _ready() -> void:
-	# Set the label nodes here
-	cardNameLabel = $TextureButton/CardNameLabel
-	cardNumberLabel = $TextureButton/CardNumberLabel
-	point_damage = 10
+    super._ready()
+    # Set the label nodes here
+    cardNameLabel = $TextureButton/CardNameLabel
+    cardNumberLabel = $TextureButton/CardNumberLabel
+    point_damage = 10
 
-	# Set the card's name and number
-	setCardName("Arrow Attack Card")
-	setCardNumber(15)
+    # Set the card's name and number
+    setCardName("Arrow Attack Card")
+    setCardNumber(15)
 
-	# If you have additional setup, handle it here
-	
+    # If you have additional setup, handle it here
+    
 func effect(actor: Actor) -> void:
-	# Decrease the health of the actor.
-	actor.onHurt(point_damage)
+    # Decrease the health of the actor.
+    actor.onHurt(point_damage)
